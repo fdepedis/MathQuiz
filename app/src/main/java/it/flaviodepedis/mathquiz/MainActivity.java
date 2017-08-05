dfda6327ff6007d308e8e526582f17b5959aebe3
fdepedis <flavio.depedis@gmail.com>
2017-08-05 17:38:15 +0200

    final release

--- app/src/main/java/it/flaviodepedis/mathquiz/MainActivity.java
+++ app/src/main/java/it/flaviodepedis/mathquiz/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,66 +25,95 @@ public class MainActivity extends AppCompatActivity {
     // Tracks the status of the answer
     private boolean status;
 
-    private final int answer1 = 18;
-    private final int answer2 = 1;
-    private final int answer3 = 114;
-    private final int answer4 = 9;
-    private final int answer5 = 625;
-    private final boolean answer6 = true;
-
     // Declare view
     private TextView tvQuestion;
     private TextView idQuestion;
-    private EditText etAnswer;
+    private TextView tvScore;
     private Button btnSubmit;
-
-    final FrameLayout frameQ1 = (FrameLayout) findViewById(R.id.question1);
-    final FrameLayout frameQ2 = (FrameLayout) findViewById(R.id.question2);
-    final FrameLayout frameQ3 = (FrameLayout) findViewById(R.id.question3);
-    final FrameLayout frameQ4 = (FrameLayout) findViewById(R.id.question4);
-    final FrameLayout frameQ5 = (FrameLayout) findViewById(R.id.question5);
-    final FrameLayout frameQ6 = (FrameLayout) findViewById(R.id.question6);
+    private FrameLayout frameQ1;
+    private FrameLayout frameQ2;
+    private FrameLayout frameQ3;
+    private FrameLayout frameQ4;
+    private FrameLayout frameQ5;
+    private FrameLayout frameQ6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialize the view
+        frameQ1 = (FrameLayout) findViewById(R.id.question1);
+        frameQ2 = (FrameLayout) findViewById(R.id.question2);
+        frameQ3 = (FrameLayout) findViewById(R.id.question3);
+        frameQ4 = (FrameLayout) findViewById(R.id.question4);
+        frameQ5 = (FrameLayout) findViewById(R.id.question5);
+        frameQ6 = (FrameLayout) findViewById(R.id.question6);
+        tvScore = (TextView) findViewById(R.id.tvScore);
+
+        // setup all FrameLayout to INVISIBLE
+        setupViewInvisible();
+
+        // method to display the current question
         displayQuestion(countQuestion);
     }
 
     /**
      * Method called from onClick properties and handle the click event button.
      */
-    public void submitAnswer(View view){
-
-        etAnswer = (EditText) findViewById(R.id.idAnswer);
+    public void submitAnswer(View view) {
 
         // verify if EditText is Empty and the question is <=5 (number of questions)
-        if (!etAnswer.getText().toString().isEmpty() && countQuestion <= NUM_OF_QUESTIONS) {
+        if (countQuestion <= NUM_OF_QUESTIONS) {
             status = verifyAnswer(countQuestion);
             if (status) {
                 okAnswer();
-                //updateScore();
-            }
-            else {
-                Toast.makeText(getApplicationContext(),R.string.incorrectAnswer, Toast.LENGTH_SHORT).show();
+                updateScore();
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.incorrectAnswer, Toast.LENGTH_SHORT).show();
             }
         }
-        else {
-            Toast.makeText(getApplicationContext(),R.string.emptyAnswer, Toast.LENGTH_SHORT).show();
-        }
+    }
+
+    /**
+     * Method called from onClick properties and handle the click event button.
+     */
+    public void submitReset(View view) {
+        countQuestion = 1;
+        score = 0;
+        setupViewInvisible();
+        displayQuestion(countQuestion);
+        btnSubmit.setEnabled(true);
     }
 
     /**
      * Method called to display message for correct answer
      * and increase the counter of next question.
-     *
      */
-    public void okAnswer(){
-        countQuestion ++;
-        Toast.makeText(getApplicationContext(),R.string.correctAnswer, Toast.LENGTH_SHORT).show();
+    public void okAnswer() {
+        countQuestion++;
+        Toast.makeText(getApplicationContext(), R.string.correctAnswer, Toast.LENGTH_SHORT).show();
         displayQuestion(countQuestion);
+    }
+
+    /**
+     * Method for count score.
+     */
+    public void updateScore() {
+        score = score + 20;
+    }
+
+    /**
+     * Method to setup all FrameLayout to INVISIBLE.
+     */
+    public void setupViewInvisible() {
+        frameQ1.setVisibility(View.INVISIBLE);
+        frameQ2.setVisibility(View.INVISIBLE);
+        frameQ3.setVisibility(View.INVISIBLE);
+        frameQ4.setVisibility(View.INVISIBLE);
+        frameQ5.setVisibility(View.INVISIBLE);
+        frameQ6.setVisibility(View.INVISIBLE);
+        tvScore.setVisibility(View.INVISIBLE);
     }
 
     /**
@@ -90,92 +121,59 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param countQuestion identify the actual question
      */
-    public void displayQuestion(int countQuestion){
+    public void displayQuestion(int countQuestion) {
 
+        // create view reference
         tvQuestion = (TextView) findViewById(R.id.textQuestion);
         idQuestion = (TextView) findViewById(R.id.idQuestion);
-        etAnswer = (EditText) findViewById(R.id.idAnswer);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
         // find which question need to show
-        if (countQuestion == 1) {
-            //question = question1;
-            tvQuestion.setText(R.string.question1);
-            idQuestion.setText(Integer.toString(countQuestion));
-            frameQ1.setVisibility(View.VISIBLE);
-            frameQ2.setVisibility(View.INVISIBLE);
-            frameQ3.setVisibility(View.INVISIBLE);
-            frameQ4.setVisibility(View.INVISIBLE);
-            frameQ5.setVisibility(View.INVISIBLE);
-            frameQ6.setVisibility(View.INVISIBLE);
-        }
-        else if (countQuestion == 2) {
-            //question = question2;
-            tvQuestion.setText(R.string.question2);
-            idQuestion.setText(Integer.toString(countQuestion));
-            frameQ1.setVisibility(View.INVISIBLE);
-            frameQ2.setVisibility(View.VISIBLE);
-            frameQ3.setVisibility(View.INVISIBLE);
-            frameQ4.setVisibility(View.INVISIBLE);
-            frameQ5.setVisibility(View.INVISIBLE);
-            frameQ6.setVisibility(View.INVISIBLE);
-        }
-        else if (countQuestion == 3) {
-            //question = question3;
-            tvQuestion.setText(R.string.question3);
-            idQuestion.setText(Integer.toString(countQuestion));
-            frameQ1.setVisibility(View.INVISIBLE);
-            frameQ2.setVisibility(View.INVISIBLE);
-            frameQ3.setVisibility(View.VISIBLE);
-            frameQ4.setVisibility(View.INVISIBLE);
-            frameQ5.setVisibility(View.INVISIBLE);
-            frameQ6.setVisibility(View.INVISIBLE);
-        }
-        else if (countQuestion == 4) {
-            //question = question4;
-            tvQuestion.setText(R.string.question4);
-            idQuestion.setText(Integer.toString(countQuestion));
-            frameQ1.setVisibility(View.INVISIBLE);
-            frameQ2.setVisibility(View.INVISIBLE);
-            frameQ3.setVisibility(View.INVISIBLE);
-            frameQ4.setVisibility(View.VISIBLE);
-            frameQ5.setVisibility(View.INVISIBLE);
-            frameQ6.setVisibility(View.INVISIBLE);
-        }
-        else if (countQuestion == 5) {
-            //question = question5;
-            tvQuestion.setText(R.string.question5);
-            idQuestion.setText(Integer.toString(countQuestion));
-            frameQ1.setVisibility(View.INVISIBLE);
-            frameQ2.setVisibility(View.INVISIBLE);
-            frameQ3.setVisibility(View.INVISIBLE);
-            frameQ4.setVisibility(View.INVISIBLE);
-            frameQ5.setVisibility(View.VISIBLE);
-            frameQ6.setVisibility(View.INVISIBLE);
-        }
-        else if (countQuestion == 6) {
-            //question = question6;
-            tvQuestion.setText(R.string.question6);
-            idQuestion.setText(Integer.toString(countQuestion));
-            frameQ1.setVisibility(View.INVISIBLE);
-            frameQ2.setVisibility(View.INVISIBLE);
-            frameQ3.setVisibility(View.INVISIBLE);
-            frameQ4.setVisibility(View.INVISIBLE);
-            frameQ5.setVisibility(View.INVISIBLE);
-            frameQ6.setVisibility(View.VISIBLE);
-        }
-        else{
-            tvQuestion.setText(R.string.completedQuiz);
-            idQuestion.setText(R.string.resetQuestions);
-            etAnswer.setEnabled(false);
-            etAnswer.setHint(R.string.resetQuestions);
-            btnSubmit.setEnabled(false);
-            frameQ1.setVisibility(View.INVISIBLE);
-            frameQ2.setVisibility(View.INVISIBLE);
-            frameQ3.setVisibility(View.INVISIBLE);
-            frameQ4.setVisibility(View.INVISIBLE);
-            frameQ5.setVisibility(View.INVISIBLE);
-            frameQ6.setVisibility(View.INVISIBLE);
+        switch (countQuestion) {
+            case 1:
+                tvQuestion.setText(R.string.question1);
+                idQuestion.setText(Integer.toString(countQuestion));
+                frameQ1.setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                tvQuestion.setText(R.string.question2);
+                idQuestion.setText(Integer.toString(countQuestion));
+                frameQ1.setVisibility(View.INVISIBLE);
+                frameQ2.setVisibility(View.VISIBLE);
+                break;
+            case 3:
+                tvQuestion.setText(R.string.question3);
+                idQuestion.setText(Integer.toString(countQuestion));
+                frameQ2.setVisibility(View.INVISIBLE);
+                frameQ3.setVisibility(View.VISIBLE);
+                break;
+            case 4:
+                tvQuestion.setText(R.string.question4);
+                idQuestion.setText(Integer.toString(countQuestion));
+                frameQ3.setVisibility(View.INVISIBLE);
+                frameQ4.setVisibility(View.VISIBLE);
+                break;
+            case 5:
+                tvQuestion.setText(R.string.question5);
+                idQuestion.setText(Integer.toString(countQuestion));
+                frameQ4.setVisibility(View.INVISIBLE);
+                frameQ5.setVisibility(View.VISIBLE);
+                break;
+            case 6:
+                tvQuestion.setText(R.string.question6);
+                idQuestion.setText(Integer.toString(countQuestion));
+                frameQ5.setVisibility(View.INVISIBLE);
+                frameQ6.setVisibility(View.VISIBLE);
+                break;
+            default:
+                tvQuestion.setText(R.string.completedQuiz);
+                idQuestion.setText(R.string.resetQuestions);
+                setupViewInvisible();
+                tvScore.setVisibility(View.VISIBLE);
+                String msg = getString(R.string.score) + " " + String.valueOf(score);
+                tvScore.setText(msg);
+                btnSubmit.setEnabled(false);
+                break;
         }
     }
 
@@ -184,38 +182,50 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param countQuestion identify the actual question
      */
-    public boolean verifyAnswer(int countQuestion){
+    public boolean verifyAnswer(int countQuestion) {
 
-        etAnswer = (EditText) findViewById(R.id.idAnswer);
-
-        if (countQuestion == 1 && Integer.parseInt(etAnswer.getText().toString()) == answer1) {
-            etAnswer.setText(R.string.resetQuestions);
-            return true;
-        }
-        else if (countQuestion == 2 && Integer.parseInt(etAnswer.getText().toString()) == answer2) {
-            etAnswer.setText(R.string.resetQuestions);
-            return true;
-        }
-        else if (countQuestion == 3 && Integer.parseInt(etAnswer.getText().toString()) == answer3) {
-            etAnswer.setText(R.string.resetQuestions);
-            return true;
-        }
-        else if (countQuestion == 4 && Integer.parseInt(etAnswer.getText().toString()) == answer4) {
-            etAnswer.setText(R.string.resetQuestions);
-            return true;
-        }
-        else if (countQuestion == 5 && Integer.parseInt(etAnswer.getText().toString()) == answer5) {
-            etAnswer.setText(R.string.resetQuestions);
-            return true;
-        }
-        /*
-        else if (countQuestion == 6 && etAnswer.getText().toString()) == answer6) {
-            etAnswer.setText(R.string.resetQuestions);
-            return true;
-        }
-        */
-        else{
+        if (countQuestion == 1) {
+            RadioButton hasBtn1_2 = (RadioButton) findViewById(R.id.radioBtn1_2);
+            if (hasBtn1_2.isChecked()) {
+                updateScore();
+                return true;
+            }
+        } else if (countQuestion == 2) {
+            EditText etAnswer = (EditText) findViewById(R.id.textAnswer);
+            if (!etAnswer.getText().toString().isEmpty() && etAnswer.getText().toString().equals("1")) {
+                updateScore();
+                return true;
+            }
+        } else if (countQuestion == 3) {
+            RadioButton hasBtn3_2 = (RadioButton) findViewById(R.id.radioBtn3_2);
+            if (hasBtn3_2.isChecked()) {
+                updateScore();
+                return true;
+            }
+        } else if (countQuestion == 4) {
+            CheckBox hasCheckBox3 = (CheckBox) findViewById(R.id.checkbox4_3);
+            CheckBox hasCheckBox4 = (CheckBox) findViewById(R.id.checkbox4_4);
+            if (hasCheckBox3.isChecked() && hasCheckBox4.isChecked()) {
+                updateScore();
+                return true;
+            }
+        } else if (countQuestion == 5) {
+            CheckBox hasCheckBox1 = (CheckBox) findViewById(R.id.checkbox5_1);
+            CheckBox hasCheckBox3 = (CheckBox) findViewById(R.id.checkbox5_3);
+            if (hasCheckBox1.isChecked() && hasCheckBox3.isChecked()) {
+                updateScore();
+                return true;
+            }
+        } else if (countQuestion == 6) {
+            RadioButton hasBtn6_1 = (RadioButton) findViewById(R.id.radioBtn6_1);
+            if (hasBtn6_1.isChecked()) {
+                updateScore();
+                return true;
+            }
+        } else {
             return false;
         }
+        return false;
     }
+
 }
